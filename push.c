b1cd6dfbc1d144882f765f31b8948c766b384813
Irene Muthoni <111053155+Irene1c@users.noreply.github.com>
2023-03-24 06:46:54 -0700

    The opcode push pushes an element to the stack

---
+++ push.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * _push - push an element to the stack
+ * @stack: pointer to the head of the stack
+ * @line_number: number of lines
+ */
+void _push(stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+	int number;
+	stack_t *new;
+
+	if (num == NULL || !isdigit(num[i]))
+	{
+		i++;
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	number = atoi(num);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	new->n = number;
+	if (*stack == NULL)
+	{
+		new->next = NULL;
+		new->prev = NULL;
+		*stack = new;
+	}
+	else
+	{
+		new->next = *stack;
+		new->prev = NULL;
+		*stack = new;
+	}
+}
